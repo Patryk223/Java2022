c38927068e3b840b26d2ed26f32fb5b9aefb74af
Patryk Krajnik <patryk.krajnik1999@gmail.com>
2022-04-13 17:29:44 +0200

    main

--- src/ekstraklasa/Main.java
+++ src/ekstraklasa/Main.java
@@ -1,4 +1,11 @@
 package ekstraklasa;
 
+import java.util.Scanner;
+
 public class Main {
+    static public void main(String[] args){
+        Klasa3 główna = new Klasa3();
+        System.out.print("Wynik: " );
+        główna.wyswietl();
+    }
 }
